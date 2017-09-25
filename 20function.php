a2f5b74d97799b152f35eadc4d8a4236d107d86d
jie cai <jc926@afsconnect1.njit.edu>
2017-09-25 18:50:53 -0400

    array1

--- 20function.php
+++ 20function.php
@@ -1,5 +1,8 @@
 <?php
+echo '<h1> 10 Array Function </h1>';
+$Array_fill = array_fill(1, 3, 'jie cai');
+echo'<h2> 1.Array_fill function </h2></br>';
 
-echo 'Hi everyone'
-
+print_r($Array_fill);
+echo '<hr>';
  ?>
