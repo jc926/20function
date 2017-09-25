ce3205f0458d3319f0040792b783be3cd4df9574
jie cai <jc926@afsconnect1.njit.edu>
2017-09-25 19:13:56 -0400

    array2

--- 20function.php
+++ 20function.php
@@ -1,8 +1,13 @@
 <?php
 echo '<h1> 10 Array Function </h1>';
+
 $Array_fill = array_fill(1, 3, 'jie cai');
 echo'<h2> 1.Array_fill function </h2></br>';
-
 print_r($Array_fill);
+echo '<hr>';
+
+$Random_array = array ('I','am','learning','php','and','html','.');
+echo '<h2> 2.Array_chunk function </h2></br>';
+print_r(array_chunk($Random_array, 3, true));
 echo '<hr>';
  ?>
